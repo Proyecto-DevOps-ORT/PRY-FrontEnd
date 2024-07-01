8147da70e6cc750652b9594f2d076e173ea57cd9
Maximiliano Lain <maximiliano_lain3@hotmail.com>
2024-06-30 21:02:55 -0300

    Try Fix

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Add node_modules/.bin to PATH
+export PATH=$PATH:./node_modules/.bin
+
 #### Running build ####
 export COMMIT_ID=$(git log --pretty="%h" --no-merges -1)
 export COMMIT_DATE="$(git log --date=format:'%Y-%m-%d %H:%M:%S' --pretty="%cd" --no-merges -1)"
